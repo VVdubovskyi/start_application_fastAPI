1efea494035443d3c939e78dcb3e2a6ae1ef7331
Aim4ik <vvdubovskyi@gmail.com>
2024-08-13 20:09:34 +0300

    Create main settings

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
+from api import router as api_router
+from core.config import settings
 
 app = FastAPI()
+app.include_router(api_router, prefix=settings.api_prefix.prefix)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
-
-
-
+    uvicorn.run("main:app", host=settings.run.host, port=settings.run.port, reload=True)
